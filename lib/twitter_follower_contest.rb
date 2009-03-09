950493c5d10457bbbe218850608a62cbf60a4c4e
Dan Croak <dcroak@thoughtbot.com>
2009-03-09 17:31:06 -0400

    lib, test

---
+++ lib/twitter_follower_contest.rb
@@ -0,0 +1,56 @@
+require 'rubygems'
+require 'rest_client'
+require 'nokogiri'
+require 'open-uri'
+
+PER_PAGE = 100
+
+class User
+  attr_accessor :name, :password, :followers
+
+  def initialize(opts = {})
+    @name      = opts[:name]
+    @password  = opts[:password]
+    find_followers
+  end
+
+  def find_followers
+    page_count = (followers_count.to_f / PER_PAGE.to_f).ceil
+    @followers ||= []
+    page_count.times do |page|
+      @followers += followers_for_page(page)
+    end
+    @followers
+  end
+
+  def followers_for_page(page)
+    followers_path = "http://twitter.com/statuses/followers.xml?page=#{page}"
+    resource       = RestClient::Resource.new(followers_path, name, password)
+
+    Nokogiri::XML(resource.get).xpath("//screen_name").collect do |each| 
+      each.inner_html.to_s.strip
+    end
+  end
+
+  def random_follower
+    followers[rand(followers.size)]
+  end
+
+  def followers_count
+    user_path = "http://twitter.com/users/show/#{name}.xml"
+    resource  = RestClient::Resource.new(user_path, name, password)
+
+    Nokogiri::XML(resource.get).xpath("//followers_count").collect do |each| 
+      each.inner_html.to_s.strip
+    end.first.to_i
+  end
+
+end
+
+credentials_path = File.join(ENV['HOME'], ".twitter.yml")
+credentials      = YAML::load_file(credentials_path)
+user = User.new(:name     => credentials['name'], 
+                :password => credentials['password'])
+
+puts user.random_follower
+
---
+++ test/twitter_follower_contest_test.rb
@@ -0,0 +1,27 @@
+require 'test/unit'
+require 'shoulda'
+require 'quietbacktrace'
+require 'redgreen'
+
+require File.join(File.dirname(__FILE__), "..", "lib", "twitter_follower_contest")
+
+class UserTest < Test::Unit::TestCase
+  context "a User with valid credentials" do
+    setup do
+      credentials_path = File.join(ENV['HOME'], ".twitter.yml")
+      credentials      = YAML::load_file(credentials_path)
+      @user = User.new(:name     => credentials['name'], 
+                       :password => credentials['password'])
+    end
+
+    should "have equal number of followers as their official follower count" do
+      assert_equal @user.followers_count, @user.followers.size
+    end
+
+    should "pick a random follower" do
+      assert @user.followers.any?
+      assert @user.followers.include?(@user.random_follower)
+    end
+  end
+end
+
